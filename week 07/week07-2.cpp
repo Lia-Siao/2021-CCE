ff070ba8ef06ca9d84f20c2a5bee45a00464b9d7
Lia-Siao <91170844+Lia-Siao@users.noreply.github.com>
2021-10-27 11:49:57 +0800

    Add files via upload

---
+++ week 07/week07-2.cpp
@@ -0,0 +1,12 @@
+///Week07 step01-2 複習 if(判斷) 及新的 while(迴圈)
+#include <stdio.h>
+int main()
+{
+    int n=10;
+//  if(n>0){
+//    printf("n大於0\n");
+//  }
+    while(n>0){
+        printf("n大於0\n");
+    }
+}
