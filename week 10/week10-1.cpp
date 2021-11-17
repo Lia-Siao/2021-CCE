b8c837b1359a9772188b0790a80c43831d80faa5
Lia-Siao <91170844+Lia-Siao@users.noreply.github.com>
2021-11-17 11:52:15 +0800

    Add files via upload

---
+++ week 10/week10-1.cpp
@@ -0,0 +1,19 @@
+///Week10-1 step01-1 计借计Τ碭
+#include <stdio.h>
+int main()
+{
+    int m;
+    scanf("%d",&m);
+    int ans=0;
+    for(int n=2;n<=m;n++){
+        int bad=0;
+        for(int i=2; i<n; i++){
+            if(n%i==0)bad=1;
+        }
+        if(bad==0){///т1腐借计
+            ans++;///借计舘!!!
+            printf("%d ",n);
+        }
+    }
+    printf("\n Ans: %d \n",ans);
+}
